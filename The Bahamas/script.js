af907bd1ed090e9abc56a72322bc8dff1f12aa7d
cameronc67 <cameronc83@nycstudents.net>
2025-12-11 13:03:44 -0500

    iu

--- The Bahamas/script.js
+++ The Bahamas/script.js
@@ -1,1 +1,24 @@
-header
+header;{
+    background-color 
+    text-align; center
+    padding: 10;px
+}
+
+display; flex;
+list-style-type; none;
+margin; 0;
+padding; 0;
+background-color
+color; black
+
+ background-image; url(img_bahamas.jpg);
+  background-position; right; top;
+  background-repeat; no-repeat;
+  background-size; 100;px; 80;px;
+ 
+  background-image; url(img_Bahamas.jpg);
+  background-position; Left; top;
+  background-repeat; no-repeat;
+  background-size; 100;px; 80;px;
+
+  
